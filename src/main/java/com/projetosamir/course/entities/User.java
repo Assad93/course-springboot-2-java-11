9a14f6ac3e5a1815142087252163a03c78144d7a
SamirAssad-coder <samir_caverna@hotmail.com>
2020-02-14 11:04:56 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetosamir/course/entities/User.java
+++ src/main/java/com/projetosamir/course/entities/User.java
@@ -2,12 +2,19 @@ package com.projetosamir.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable { // Serializable: serve para transformar meu obj em cadeia de bytes
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L; // numero de serie
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	private String name;
 	private String email;
